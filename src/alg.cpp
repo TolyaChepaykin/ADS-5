614f49e55bb5387b1673967a82dbc3bfee16927f
TolyaChepaykin <160387810+TolyaChepaykin@users.noreply.github.com>
2024-04-14 19:25:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,69 +3,77 @@
 #include <map>
 #include "tstack.h"
 
+int prioritet(char a) {
+  if (a == '+' || a == '-')
+    return 1;
+  if (a == '*' || a == '/')
+    return 2;
+  return 0;
+}
+
+bool isZnak(char a) {
+  return (a == '+' || a == '-' || a == '*' || a == '/');
+}
+
 std::string infx2pstfx(std::string inf) {
-    TStack<char, 100> stack1;
-    std::string pst;
-    for (int i = 0; i < inf.length(); i++) {
-        if (inf[i] >= '0' && inf[i] <= '9') {
-            pst += inf[i];
-            pst += ' ';
-        } else if (inf[i] == '(') {
-            stack1.push('(');
-        } else if (inf[i] == ')') {
-            while (!stack1.isempty() && stack1.get() != '(') {
-                pst += stack1.get();
-                pst += ' ';
-                stack1.pop();
-            }
-            if (!stack1.isempty()) {
-                stack1.pop();
-            }
-        } else {
-            while (!stack1.isempty() && stack1.get() != '(' &&
-            (inf[i] =='+' || stack1.get() =='*' || stack1.get() == '/')) {
-                pst += stack1.get();
-                pst += ' ';
-                stack1.pop();
-            }
-            stack1.push(inf[i]);
-        }
-    }
-    while (!stack1.isempty()) {
-        pst += stack1.get();
-        pst += ' ';
+  std::string inf2;
+  TStack<char, 100> stack1;
+  for (char i : inf) {
+    if (i >= '0' && i <= '9') {
+      inf2 = inf2 + i + ' ';
+    } else if (i == '(') {
+      stack1.push(i);
+    } else if (isZnak(i)) {
+      while (!stack1.isEmpty() && prioritet(stack1.get()) >= prioritet(i)) {
+        inf2 = inf2 + stack1.get() + ' ';
         stack1.pop();
+      }
+      stack1.push(i);
+    } else if (i == ')') {
+      while (!stack1.isEmpty() && stack1.get() != '(') {
+        inf2 = inf2 + stack1.get() + ' ';
+        stack1.pop();
+      }
+      stack1.pop();
     }
-    if (!pst.empty()) {
-        pst.erase(pst.size() - 1);
-    }
-    return pst;
+  }
+  while (!stack1.isEmpty()) {
+    inf2 = inf2 + stack1.get() + ' ';
+    stack1.pop();
+  }
+  if (!inf2.empty()) {
+    inf2.pop_back();
+  }
+  return inf2;
 }
 
 int eval(std::string pref) {
-    TStack<int, 100> stack2;
-    for (int i = 0; i < pref.length(); i++) {
-        char ch = pref[i];
-        if (isdigit(ch)) {
-            stack2.push(ch - '0');
-        } else if (ch == '+' || ch == '-' || ch == '/' || ch == '*') {
-            int operand2 = stack2.pop();
-            int operand1 = stack2.pop();
-            switch (ch) {
-                case '+':
-                    stack2.push(operand1 + operand2);
-                    break;
-                case '-':
-                    stack2.push(operand1 - operand2);
-                    break;
-                case '*':
-                    stack2.push(operand1 * operand2);
-                    break;
-                case '/':
-                    stack2.push(operand1 / operand2);
-                    break;
-            }
-        }
+  TStack<int, 100> stack2;
+  for (char i : pref) {
+    if (i >= '0' && i <= '9') {
+      stack2.push(i - '0');
+    } else if (isZnak(i)) {
+      int x = stack2.get();
+      stack2.pop();
+      int y = stack2.get();
+      stack2.pop();
+      switch (i) {
+        case '+':
+          stack2.push(y + x);
+          break;
+        case '-':
+          stack2.push(y - x);
+          break;
+        case '*':
+          stack2.push(y * x);
+          break;
+        case '/':
+          stack2.push(y / x);
+          break;
+        default:
+          continue;
+      }
     }
-    return stack2.pop();
+  }
+  return stack2.get();
 }
